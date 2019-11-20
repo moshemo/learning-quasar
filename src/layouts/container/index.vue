41a8a6f2efb1c99e6016fab3f86bb745a0dda85a
moshemo <moshem@live.com>
2019-11-20 22:34:01 +0200

    Sidebar Search Bar (in progress)

--- src/layouts/container/index.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <q-page-container>
-    <router-view />
-  </q-page-container>
-</template>
--- src/layouts/main/right-drawer.vue
+++ src/layouts/main/right-drawer.vue
@@ -1,5 +1,22 @@
 <template>
-  <q-drawer show-if-above side="right" content-class="bg-grey-2" v-if="isOpen">
+  <q-drawer
+    show-if-above
+    side="right"
+    content-class="bg-grey-2"
+    width="350"
+    v-if="isOpen"
+  >
+    <div class="q-py-md q-px-lg bg-grey-3">
+      <div>
+      <q-input dense standout bg-color="grey-2" label="Search" v-model="text">
+        <template v-slot:prepend>
+           <q-icon name="search" />
+        </template>
+                <template v-slot:append>
+           <q-btn />
+        </template>
+      </q-input>
+    </div>
     <q-list>
       <q-item-label header>Navigation</q-item-label>
       <q-item to="/" class="q-px-lg">
